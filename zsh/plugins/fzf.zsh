e3523534a7a536faf761512cab4936c2b9ba3a18
Robert Beilich <robert@beilich.de>
2017-12-11 21:39:18 +0100

    (ZSH): Add initial config

---
+++ zsh/plugins/fzf.zsh
@@ -0,0 +1,76 @@
+#!/usr/bin/zsh
+# https://github.com/junegunn/fzf/wiki/examples
+
+fo() {
+  local out file key
+  out=($(fzf-tmux --query="$1" --exit-0 --expect=ctrl-o,ctrl-e))
+  key=$(head -1 <<< "$out")
+  file=$(head -2 <<< "$out" | tail -1)
+  if [ -n "$file" ]; then
+    [ "$key" = ctrl-o ] && gio open "$file" || ${EDITOR:-vim} "$file"
+  fi
+}
+
+fd() {
+  local dir
+  dir=$(find ${1:-.} -path '*/\.*' -prune \
+                  -o -type d -print 2> /dev/null | fzf +m) &&
+  cd "$dir"
+}
+
+fda() {
+  local dir
+  dir=$(find ${1:-.} -type d 2> /dev/null | fzf +m) && cd "$dir"
+}
+
+fdr() {
+  local declare dirs=()
+  get_parent_dirs() {
+    if [[ -d "${1}" ]]; then dirs+=("$1"); else return; fi
+    if [[ "${1}" == '/' ]]; then
+      for _dir in "${dirs[@]}"; do echo $_dir; done
+    else
+      get_parent_dirs $(dirname "$1")
+    fi
+  }
+  local DIR=$(get_parent_dirs $(realpath "${1:-$PWD}") | fzf-tmux --tac)
+  cd "$DIR"
+}
+
+fh() {
+  print -z $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed 's/ *[0-9]* *//')
+}
+
+fkill() {
+  local pid
+  pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
+
+  if [ "x$pid" != "x" ]
+  then
+    echo $pid | xargs kill -${1:-9}
+  fi
+}
+
+tm() {
+  [[ -n "$TMUX" ]] && change="switch-client" || change="attach-session"
+  if [ $1 ]; then 
+    tmux $change -t "$1" 2>/dev/null || (tmux new-session -d -s $1 && tmux $change -t "$1"); return
+  fi
+  session=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf --exit-0) &&  tmux $change -t "$session" || echo "No sessions found."
+}
+
+
+c() {
+  local cols sep google_history open
+  cols=$(( COLUMNS / 3 ))
+  sep='{::}'
+
+  google_history="$HOME/.config/google-chrome/Default/History"
+  open=google-chrome-stable
+  cp -f "$google_history" /tmp/h
+  sqlite3 -separator $sep /tmp/h \
+    "select substr(title, 1, $cols), url
+     from urls order by last_visit_time desc" |
+  awk -F $sep '{printf "%-'$cols's  \x1b[36m%s\x1b[m\n", $1, $2}' |
+  fzf --ansi --multi | sed 's#.*\(https*://\)#\1#' | xargs $open > /dev/null 2> /dev/null
+}
---
+++ zshrc
@@ -0,0 +1,67 @@
+bindkey -v
+
+export COMPLETION_WAITING_DOTS="true"
+setopt correct
+unsetopt correctall
+setopt autocd
+unsetopt beep
+setopt pushd_ignore_dups
+setopt notify
+setopt nomatch
+setopt extendedglob
+
+export VISUAL=vim
+export EDITOR="$VISUAL"
+
+export DEFAULT_USER=sebaran
+export ZSH_CUSTOM=$HOME/.config/zsh
+export ZSH_THEME=agnoster
+
+# History
+setopt append_history
+setopt extended_history
+setopt hist_expire_dups_first
+setopt hist_ignore_all_dups
+setopt hist_ignore_dups
+setopt hist_ignore_space
+setopt hist_reduce_blanks
+setopt hist_save_no_dups
+setopt hist_verify
+setopt share_history
+HISTFILE=~/.histfile
+HISTSIZE=100000
+SAVEHIST=100000
+HIST_IGNORE="(ls|cd|cd -|..*|pwd|exit|clear|date|* --help|vim|tmux)"
+
+zstyle :compinstall filename '/home/sebaran/.zshrc'
+autoload -Uz compinit
+compinit
+
+if [ ! -d ${HOME}/.zgen ]; then
+  git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+fi
+
+source "${HOME}/.zgen/zgen.zsh"
+if ! zgen saved; then
+  zgen oh-my-zsh
+  zgen oh-my-zsh plugins/archlinux
+  zgen load zsh-users/zsh-syntax-highlighting
+  zgen load StackExchange/blackbox
+  zgen load chrissicool/zsh-256color
+  zgen load supercrabtree/k
+
+  zgen save
+fi
+
+export PATH=$PATH:$HOME/bin
+export PATH=$PATH:$HOME/.config/composer/vendor/bin
+
+for plugin in ~/.config/zsh/plugins/*.zsh; do
+  source $plugin
+done
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
+
+silent(){ 2>/dev/null 1>&2 $* }
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
